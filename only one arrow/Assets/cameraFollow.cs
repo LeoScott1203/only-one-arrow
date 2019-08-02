0460048c7a86822a601db62955dde345491532e4
LeoScott1203 <LeoScott03@hotmail.com>
2019-08-02 18:35:43 -0400

    player turns toward mouse and changed movement

--- only one arrow/Assets/cameraFollow.cs
+++ only one arrow/Assets/cameraFollow.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraFollow : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
 
     public Transform player;
--- only one arrow/Assets/playerMovement.cs
+++ only one arrow/Assets/playerMovement.cs
@@ -2,25 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 0.1f;
+
+    Vector3 mousePosition;
+    Vector2 lookDirection;
 
     void Update()
     {
 
         if (Input.GetKey(KeyCode.A))
-            transform.Translate(Vector2.left * speed);
+            transform.position += Vector3.left * speed;
 
         if (Input.GetKey(KeyCode.D))
-            transform.Translate(Vector2.right * speed);
+            transform.position += Vector3.right * speed;
 
         if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector2.up * speed);
+            transform.position += Vector3.up * speed;
 
         if (Input.GetKey(KeyCode.S))
-            transform.Translate(Vector2.down * speed);
+            transform.position += Vector3.down * speed;
+
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lookDirection = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        transform.up = lookDirection;
 
     }
 
