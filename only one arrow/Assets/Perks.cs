c918ba7c5a458b22f46325369c8bdf90607b86a7
UristMcDorf <semyonovid@gmail.com>
2019-08-03 00:35:54 +0300

    - Perks controller. Should do the job. New perks should be added in the Perk enum.

---
+++ only one arrow/Assets/Perks.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+public static class Perks
+{
+    static BitArray unlockedPerks = new BitArray((int)Perk.Count);
+
+    public static void Unlock(Perk perk)
+    {
+        unlockedPerks[(int)perk] = true;
+    }
+
+    public static bool IsUnlocked(Perk perk)
+    {
+        return unlockedPerks[(int)perk];
+    }
+}
+
+public enum Perk
+{
+    // Always add new perks before Count so that it stays up to date in a proper way - Urist
+    GenericTestPerk,
+    Count
+}
