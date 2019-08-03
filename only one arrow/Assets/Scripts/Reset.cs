2f05dde46d2a7a1aac76268a34982bfd6d16bdf1
LeoScott1203 <LeoScott03@hotmail.com>
2019-08-03 13:52:59 -0400

    game over added

---
+++ only one arrow/Assets/Scripts/Reset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+
+    public void reset()
+    {
+        Application.LoadLevel(0);
+    }
+
+}
