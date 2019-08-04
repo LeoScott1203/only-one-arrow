41ae2b736a99b17c84c0cccb87a5bc071e6f3721
LeoScott1203 <LeoScott03@hotmail.com>
2019-08-04 12:09:44 -0400

    main menu

---
+++ only one arrow/Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+
+    public void onButtonPressed()
+    {
+
+        Application.LoadLevel(1);
+
+    }
+
+}
